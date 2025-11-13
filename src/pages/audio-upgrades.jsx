2224fcc1b63c2a1cfd64631df3747bb4626faf20
J E F <jefaraz@gmail.com>
2025-11-13 12:16:15 +0000

    Manual code change

--- src/pages/audio-upgrades.jsx
+++ src/pages/audio-upgrades.jsx
@@ -216,7 +216,7 @@ export default function AudioUpgrades() {
         <div className="max-w-7xl mx-auto px-6">
           <div className="w-full">
             <h1 className="text-[2.7rem] sm:text-[3.6rem] md:text-[4.5rem] lg:text-[4.8rem] font-extrabold text-black mb-6 leading-[1.1] slide-up-1">Audio Upgrades:
-Sound in perfect match with your space
+<br/>Sound in perfect match with your space
 
             </h1>
             
