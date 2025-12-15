ae91d89ff821a74015afa7601f857dc45b8ee045
J E F <jefaraz@gmail.com>
2025-12-15 13:30:23 +0000

    Deleted empty test file

--- src/pages/Home-test-new.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function HomeTestNew() {
-  return (
-    <div className="min-h-screen bg-[#faebe3] flex items-center justify-center">
-      <p>This is a test page</p>
-    </div>
-  );
-}
