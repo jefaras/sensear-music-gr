bf73086c509c08b97848b88fad5d716b9f5cc5d6
J E F <jefaraz@gmail.com>
2025-12-15 13:53:51 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -14,29 +14,32 @@ import PlaylistsDemos from './pages/PlaylistsDemos';
 import servicesV2 from './pages/Services-v2';
 import Services from './pages/Services';
 import Sitemap from './pages/Sitemap';
+import artMuseumsFashionPrev from './pages/art-museums-fashion-prev';
 import artMuseumsFashion from './pages/art-museums-fashion';
 import audioUpgrades from './pages/audio-upgrades';
 import backgroundMusicShapesCustomerBehavior from './pages/background-music-shapes-customer-behavior';
+import beachHouseAntiparos from './pages/beach-house-antiparos';
+import blueBamboo from './pages/blue-bamboo';
 import brandMusicConvertsBrowsersBuyers from './pages/brand-music-converts-browsers-buyers';
 import buildingBrandPeopleCanHear from './pages/building-brand-people-can-hear';
 import eventSoundtracks from './pages/event-soundtracks';
+import eventsExperiencesPrev from './pages/events-experiences-prev';
 import eventsExperiences from './pages/events-experiences';
+import hotelsResortsPrev from './pages/hotels-resorts-prev';
 import hotelsResorts from './pages/hotels-resorts';
 import howTopHospitalityBrandsDesignSound from './pages/how-top-hospitality-brands-design-sound';
+import klouviBar from './pages/klouvi-bar';
 import musicCurationCycleVenues from './pages/music-curation-cycle-venues';
 import musicForHotelsAndResorts from './pages/music-for-hotels-and-resorts';
 import musicForRestaurantsAndBars from './pages/music-for-restaurants-and-bars';
-import restaurantsBarsPrev from './pages/restaurants-bars-prev';
+import pelicanosSifnosRestaurant from './pages/pelicanos-sifnos-restaurant';
 import restaurantsBars from './pages/restaurants-bars';
-import retailStoresPrev from './pages/retail-stores-prev';
 import retailStores from './pages/retail-stores';
 import serviceEnvironmentShapesWaitTime from './pages/service-environment-shapes-wait-time';
-import signaturePlaylistsTest from './pages/signature-playlists-test';
 import signaturePlaylists from './pages/signature-playlists';
 import sonicStrategy from './pages/sonic-strategy';
 import threeReasonsMakeMusicHospitality from './pages/three-reasons-make-music-hospitality';
 import wellnessCenters from './pages/wellness-centers';
-import wellnessGymsPrev from './pages/wellness-gyms-prev';
 import wellnessGyms from './pages/wellness-gyms';
 import whatExactlyDoesMusicCuratorDo from './pages/what-exactly-does-music-curator-do';
 import __Layout from './Layout.jsx';
@@ -59,29 +62,32 @@ export const PAGES = {
     "Services-v2": servicesV2,
     "Services": Services,
     "Sitemap": Sitemap,
+    "art-museums-fashion-prev": artMuseumsFashionPrev,
     "art-museums-fashion": artMuseumsFashion,
     "audio-upgrades": audioUpgrades,
     "background-music-shapes-customer-behavior": backgroundMusicShapesCustomerBehavior,
+    "beach-house-antiparos": beachHouseAntiparos,
+    "blue-bamboo": blueBamboo,
     "brand-music-converts-browsers-buyers": brandMusicConvertsBrowsersBuyers,
     "building-brand-people-can-hear": buildingBrandPeopleCanHear,
     "event-soundtracks": eventSoundtracks,
+    "events-experiences-prev": eventsExperiencesPrev,
     "events-experiences": eventsExperiences,
+    "hotels-resorts-prev": hotelsResortsPrev,
     "hotels-resorts": hotelsResorts,
     "how-top-hospitality-brands-design-sound": howTopHospitalityBrandsDesignSound,
+    "klouvi-bar": klouviBar,
     "music-curation-cycle-venues": musicCurationCycleVenues,
     "music-for-hotels-and-resorts": musicForHotelsAndResorts,
     "music-for-restaurants-and-bars": musicForRestaurantsAndBars,
-    "restaurants-bars-prev": restaurantsBarsPrev,
+    "pelicanos-sifnos-restaurant": pelicanosSifnosRestaurant,
     "restaurants-bars": restaurantsBars,
-    "retail-stores-prev": retailStoresPrev,
     "retail-stores": retailStores,
     "service-environment-shapes-wait-time": serviceEnvironmentShapesWaitTime,
-    "signature-playlists-test": signaturePlaylistsTest,
     "signature-playlists": signaturePlaylists,
     "sonic-strategy": sonicStrategy,
     "three-reasons-make-music-hospitality": threeReasonsMakeMusicHospitality,
     "wellness-centers": wellnessCenters,
-    "wellness-gyms-prev": wellnessGymsPrev,
     "wellness-gyms": wellnessGyms,
     "what-exactly-does-music-curator-do": whatExactlyDoesMusicCuratorDo,
 }
