17f8ca0e4fd46460e7c73e2db8ab67de43468a20
J E F <jefaraz@gmail.com>
2025-12-15 13:29:22 +0000

    File changes

---
+++ src/pages/Home27_11.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home2711() {
+  return (
+    <div className="min-h-screen bg-[#faebe3] flex items-center justify-center">
+      <p>Home 27/11 page</p>
+    </div>
+  );
+}
