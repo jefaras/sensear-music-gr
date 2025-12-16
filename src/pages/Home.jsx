743abccf321915ad364a21b4ea2804d7a8a3db00
J E F <jefaraz@gmail.com>
2025-12-16 12:37:07 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -314,7 +314,7 @@ export default function Home() {
               </h1>
               
               <div className="mb-8 max-w-xl slide-up-2">
-                <p className="text-xl sm:text-2xl md:text-[1.75rem] lg:text-[2.15rem] font-semibold text-black/70 leading-loose mb-2 italic">
+                <p className="text-xl sm:text-2xl md:text-[1.75rem] lg:text-[2.15rem] font-semibold text-black/70 mb-2 italic" style={{ lineHeight: '1.8' }}>
                   Every venue has a story.<br />
                   Let music tell yours.
                 </p>
